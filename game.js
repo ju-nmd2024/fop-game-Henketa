f974a475cb63c0f734b48dd492160964aeb09dcd
Henketa <henrik6251@gmail.com>
2024-11-15 12:14:43 +0100

    Add the 3 game states
    Added functions: startScreen, gameScreen, and endScreen.
    
    Will work more on background of each screen later.

--- game.js
+++ game.js
@@ -4,13 +4,27 @@ function setup() {
 
 function startScreen(){
   background(105,190,150);
-  textSize(95);
-  text("START",315,330);
+  textSize(25);
+  fill(255,195,0);
+  stroke(0);
+  strokeWeight(3);
+  text("Press any button (unfinished)",300,550);
 }
 
 function gameScreen(){
   background(145,180,220);
-  text("Steal the emerald",10,10);
+  textSize(30);
+  text("Steal the emerald",10,30);
+}
+
+function endScreen(){
+  background(0,0,0);
+  textFont('Verdana');
+  textSize(65);
+  fill(255);
+  stroke(0,100,190);
+  strokeWeight(5);
+  text("GAME OVER",250,250);
 }
 
 function draw() {
