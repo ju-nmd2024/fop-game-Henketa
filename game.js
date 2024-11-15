df2080513aef598407f7eebf2da7089373fcbb1b
Henketa <henrik6251@gmail.com>
2024-11-15 11:22:12 +0100

    added states
    Added 2 new states/functions, startScreen & gameScreen

--- game.js
+++ game.js
@@ -1,7 +1,18 @@
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(900, 600);
+}
+
+function startScreen(){
+  background(105,190,150);
+  textSize(95);
+  text("START",315,330);
+}
+
+function gameScreen(){
+  background(145,180,220);
+  text("Steal the emerald",10,10);
 }
 
 function draw() {
-  background(255, 140, 150);
+  startScreen();
 }
